d5602e1f83462d8f6e9fe2a8127898ace5e01a87
nikolairoshchin <99800316+nikolairoshchin@users.noreply.github.com>
2023-01-10 12:10:38 +0200

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const str = n.toString();
+  let strReverse = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+   	strReverse = strReverse + str[i];
+  };
+ 
+  return parseInt(strReverse);
 }
